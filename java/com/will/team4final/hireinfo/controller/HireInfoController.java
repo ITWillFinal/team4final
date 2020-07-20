6a06851b55bbcaa6dc01f64db36afd6a64092207
AndrewLEEJM <woals214@gmail.com>
2020-07-20 15:31:55 +0900

    수정

--- java/com/will/team4final/hireinfo/controller/HireInfoController.java
+++ java/com/will/team4final/hireinfo/controller/HireInfoController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.will.team4final.login.controller.LoginController;
 
 @Controller
+@RequestMapping("/companypage")
 public class HireInfoController {
 
 private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	@RequestMapping("/companypage/companyHome.do")
-	public String home() {
+	@RequestMapping("/companyHome.do")
+	public String companyHome() {
 		logger.info("기업페이지 홈");
 		
 		return "companypage/companyHome";
