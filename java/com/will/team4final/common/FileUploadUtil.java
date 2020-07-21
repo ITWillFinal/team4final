3fdae1d6709c2aff025092e92280bd6b3a8746b7
AndrewLEEJM <woals214@gmail.com>
2020-07-21 18:22:52 +0900

    gd

--- java/com/will/team4final/common/FileUploadUtil.java
+++ java/com/will/team4final/common/FileUploadUtil.java
@@ -23,41 +23,37 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Component
 public class FileUploadUtil {
-	private Logger logger
-		=LoggerFactory.getLogger(FileUploadUtil.class);
-	
-	public static final int PATH_PDS=1;  //자료실에 사용
-	public static final int PATH_PD_IMAGE=2; //상품업로드시 사용
-	
-	@Resource(name="fileUploadProperties")
+	private Logger logger = LoggerFactory.getLogger(FileUploadUtil.class);
+
+	public static final int PATH_PDS = 1; // 자료실에 사용
+	public static final int PATH_PERSONAL_IMAGE = 2; // 상품업로드시 사용
+
+	@Resource(name = "fileUploadProperties")
 	Properties fileUploadProps;
-	
-	public List<Map<String, Object>> fileUpload(
-			HttpServletRequest request,  int pathGb) {
-		//파일 업로드 처리 메서드		
-		MultipartHttpServletRequest multiReq 
-		= (MultipartHttpServletRequest) request;
-	
-		List<Map<String, Object>> resultList
-			=new ArrayList<Map<String,Object>>();
-		
-		Map<String, MultipartFile> fileMap=multiReq.getFileMap();
-		Iterator<String> iter=fileMap.keySet().iterator();
-		while(iter.hasNext()) {
-			String key=iter.next();
-			
-			MultipartFile tempFile=fileMap.get(key);
-			//=> 업로드된 파일을 임시파일 형태로 제공
-			
-			//업로드 한 경우에 파일크기, 이름 구하기
-			if(!tempFile.isEmpty()) {
+
+	public List<Map<String, Object>> fileUpload(HttpServletRequest request, int pathGb) {
+		// 파일 업로드 처리 메서드
+		MultipartHttpServletRequest multiReq = (MultipartHttpServletRequest) request;
+
+		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+
+		Map<String, MultipartFile> fileMap = multiReq.getFileMap();
+		Iterator<String> iter = fileMap.keySet().iterator();
+		while (iter.hasNext()) {
+			String key = iter.next();
+
+			MultipartFile tempFile = fileMap.get(key);
+			// => 업로드된 파일을 임시파일 형태로 제공
+
+			// 업로드 한 경우에 파일크기, 이름 구하기
+			if (!tempFile.isEmpty()) {
 				String originalFName = tempFile.getOriginalFilename();
-				long fileSize=tempFile.getSize();
-				
-				//변경된 파일 명 구하기
-				String fileName=getUniqueFileName(originalFName);
-				
-				//업로드 처리
+				long fileSize = tempFile.getSize();
+
+				// 변경된 파일 명 구하기
+				String fileName = getUniqueFileName(originalFName);
+
+				// 업로드 처리
 				String upPath = getUploadPath(request, pathGb);
 				File file = new File(upPath, fileName);
 				try {
@@ -68,79 +64,71 @@ public class FileUploadUtil {
 					e.printStackTrace();
 				}
 				logger.info("업로드 처리되었습니다.");
-				
+
 				Map<String, Object> map = new HashMap<String, Object>();
 				map.put("fileSize", fileSize);
 				map.put("fileName", fileName);
 				map.put("originalFileName", originalFName);
-				
+
 				resultList.add(map);
 			}
-		}//while
-		
+		} // while
+
 		return resultList;
 	}
-	
+
 	public String getUploadPath(HttpServletRequest request, int pathGb) {
-		String type=fileUploadProps.getProperty("file.upload.type");
-		//=> test, deploy
-		
-		String uploadPath="";
-		String key="";
-		if(type.equals("test")) { //테스트시
-			if(pathGb==PATH_PDS) {
-				key="file.upload.path.test";
-			}else if(pathGb==PATH_PD_IMAGE) {
-				key="imageFile.upload.path.test";
+		String type = fileUploadProps.getProperty("file.upload.type");
+		// => test, deploy
+
+		String uploadPath = "";
+		String key = "";
+		if (type.equals("test")) { // 테스트시
+			if (pathGb == PATH_PDS) {
+				key = "file.upload.path.test";
+			} else if (pathGb == PATH_PERSONAL_IMAGE) {
+				key = "imageFile.upload.path.test";
 			}
-			
-			uploadPath=fileUploadProps.getProperty(key);
-		}else { //deploy-배포시
-			if(pathGb==PATH_PDS) {
-				key="file.upload.path";
-			}else if(pathGb==PATH_PD_IMAGE) {
-				key="imageFile.upload.path";
+
+			uploadPath = fileUploadProps.getProperty(key);
+		} else { // deploy-배포시
+			if (pathGb == PATH_PDS) {
+				key = "file.upload.path";
+			} else if (pathGb == PATH_PERSONAL_IMAGE) {
+				key = "imageFile.upload.path";
 			}
-			
-			uploadPath=fileUploadProps.getProperty(key);
-			//실제 물리적인 경로 구하기
-			HttpSession session=request.getSession();
-			uploadPath
-				=session.getServletContext().getRealPath(uploadPath);
+
+			uploadPath = fileUploadProps.getProperty(key);
+			// 실제 물리적인 경로 구하기
+			HttpSession session = request.getSession();
+			uploadPath = session.getServletContext().getRealPath(uploadPath);
 		}
-		
+
 		logger.info("type={}, uploadPath={}", type, uploadPath);
-		
+
 		return uploadPath;
 	}
-	
+
 	public String getUniqueFileName(String originFName) {
-		//변경된 파일명
-		//ab.txt => ab_현재시간.txt
-		int idx=originFName.lastIndexOf(".");
-		String fname = originFName.substring(0, idx); //ab
-		String ext = originFName.substring(idx); //.txt
-		
-		String fileName = fname+"_"+getTimeStamp()+ext;
+		// 변경된 파일명
+		// ab.txt => ab_현재시간.txt
+		int idx = originFName.lastIndexOf(".");
+		String fname = originFName.substring(0, idx); // ab
+		String ext = originFName.substring(idx); // .txt
+
+		String fileName = fname + "_" + getTimeStamp() + ext;
 		logger.info("변경된 파일명 = {}", fileName);
-		
+
 		return fileName;
 	}
-	
+
 	public String getTimeStamp() {
-		//현재 시간을 밀리초까지 지정해서 리턴
+		// 현재 시간을 밀리초까지 지정해서 리턴
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 		Date d = new Date();
-		String str=sdf.format(d);
-		
+		String str = sdf.format(d);
+
 		return str;
 	}
-	
-	
+
 }
-
-
-
-
-
-
