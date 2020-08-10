03a58c66f367da6748d3f53c58a9be7da6b7173e
AndrewLEEJM <woals214@gmail.com>
2020-08-10 23:07:48 +0900

    결제정보

--- java/com/will/team4final/payment/model/PaymentService.java
+++ java/com/will/team4final/payment/model/PaymentService.java
@@ -1,6 +1,8 @@
 package com.will.team4final.payment.model;
 
+import java.util.List;
+
 public interface PaymentService {
 	int insertPayment(PaymentVO paymentVo);
-
+	String selectTodayPayment();
 }
